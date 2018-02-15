d1755f8a397d43a70a76db2a62d936a06858aeb9
Mak <makram3ibrahim@gmail.com>
2018-02-14 19:56:47 -0700

    Initial Glazer Application

---
+++ GlazerCalculation/Properties/Glazer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlazerCalculation.Properties
+{
+    class Glazer
+    {
+        private double Width      { get; set; }
+        private double Height     { get; set; }
+        private double GlassArea  { get; set; }
+        private double WoodLength { get; set; }
+
+        public Glazer(double width, double height, double glassArea, double woodLength)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.GlassArea = glassArea;
+            this.WoodLength = woodLength;
+        }
+    }
+}
